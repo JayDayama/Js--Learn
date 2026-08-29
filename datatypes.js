14ec99fdf6f14e3844b3d878296735928075aff9
JayDayama <jay.dayamaa@gmail.com>
2026-08-30 01:07:24 +0530

    something about datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treats all js as newer version
+
+// aleart(3 + 3) this is node js not a browser
+
+let name = "Jay Dayama"
+let age = 19
+let islogged = false
+
+// number => (integer)
+// bigint
+//string => ""
+//boolean => true/false
+// null -> standalone value (lets say it is empty)
+// undefined => we havent given it some value yet
+//symbol => unique(mainly used in react)
+
+//object
+
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
--- variable.js
+++ variable.js
@@ -3,6 +3,7 @@ let accountEmail = "Jaydayama@gmail.com"
 var accountPassword = "0905"
 account_city = "Ahmedabad"
 let accountState
+accstar = "Jay"
 // accountid = 42 we cant change value of a const variable
 
 //we prefer let because var has no scope limits {} something declared here can be used outside too
