7bca8983065487c767e9e7686d088f3cfa517998
Jay Dayama <jaydayama@Jays-MacBook-Air.local>
2026-08-30 00:06:24 +0530

    Variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountid = 12345
+let accountEmail = "Jaydayama@gmail.com"
+var accountPassword = "0905"
+account_city = "Ahmedabad"
+let accountState
+// accountid = 42 we cant change value of a const variable
+
+//we prefer let because var has no scope limits {} something declared here can be used outside too
+
+accountEmail = "jd@cc.com"
+accountPassword = "1242"
+account_city = "Surat"
+console.table([accountid,accountEmail,accountPassword,account_city,accountState])
+
+
+// console.log(accountid);
