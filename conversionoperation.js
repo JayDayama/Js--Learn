70b10d31df0509b540fd4d96d2ccbf6ba7fbaf00
JayDayama <jay.dayamaa@gmail.com>
2026-08-30 23:19:07 +0530

    conversionsandoperations

---
+++ conversionoperation.js
@@ -0,0 +1,47 @@
+// // let score = null
+// // let score = undefined
+// let score = "33abs"
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueinNumber = Number(score)
+
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+// //"33" => 33
+// //"33abc" =>Nan
+// //"true" => 1
+
+// // let loggedin = ""
+// let loggedin = 0
+
+// let booleanloggedin = Boolean(loggedin)
+// console.log(typeof booleanloggedin)
+// console.log(booleanloggedin)
+
+// //1 -> true ; 0 -> false
+// //"" -> false "j"->true
+
+// let num = 33
+
+// let stringnum = String(num)
+// console.log(typeof stringnum)
+// console.log(stringnum)
+
+//                              ** OPERATIONS **
+
+let str1 = "Hello"
+let str2 = " Jay"
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")//1 + 2 ->3 + "2" => 32
+// console.log(1 + 2 + "2" + 2)
+
+console.log(+true)// -> 1
+console.log(+"")// -> 0
